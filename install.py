91cd71bed5639e5c75317b5dfe81089c73f9d75c
Pirnazar <41678493+Pirnazar@users.noreply.github.com>
2021-01-29 22:22:28 +0500

    Add files via upload

--- install.py
+++ install.py
@@ -1,10 +1,11 @@
 import os
 print('Установка пакетов')
 os.system('sudo apt update')
-os.system('sudo apt upgrade')
+os.system('sudo apt upgrade -y')
 os.system('sudo apt-get install net-tools')
+os.system('pip3 install flask')
 os.system('mkdir /usr/share/outlinevpn')
-print('Установка скрипта')
+print('Installing script...')
 with open('/usr/share/outlinevpn/app.py', 'w') as f:
     f.write('''#!/usr/bin/python3
 from flask import Flask, request, jsonify
@@ -13,7 +14,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def data():
     port = request.json['port']
-    s  = os.popen("netstat -ant | grep :"+str(port)+" | grep ESTABLISHED | awk >
+    s  = os.popen("netstat -ant | grep :"+str(port)+" | grep ESTABLISHED | awk '{print $5}'").read().split()
     ips = []
     for i in s:
         i = i.split(':')
@@ -26,7 +27,7 @@ def data():
 if __name__ == '__main__':
     app.run(host='0.0.0.0')''')
 os.system('chmod 777 /usr/share/outlinevpn/app.py')
-print('Добавление срипта в автозагрузку')
+print('Adding to autostart')
 with open('/etc/systemd/system/outlinevpn.service', 'w') as f:
     f.write('''[Unit]
 After=network.target
@@ -34,10 +35,10 @@ After=network.target
 CapabilityBoundingSet=CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_BIND_SERVICE
 LimitNOFILE=51200
-ExecStart=/root/app.py
+ExecStart=/usr/share/outlinevpn/app.py
 [Install]
 WantedBy=default.target''')
-print('Запуск')
+print('Start')
 os.system('systemctl start outlinevpn')
 os.system('systemctl enable outlinevpn')
-print('Успешно')
+print('Success')
